91bf36b390408c491b0ea03d3e916505d78267f4
Shuxriddin <107147305+Shuxriddin@users.noreply.github.com>
2025-12-27 20:33:52 +0500

    Update serializer.py

--- app/serializer.py
+++ app/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from .models import BotUserModel,TelegramChannelModel, Movie
+from .models import BotUserModel, TelegramChannelModel, Movie, Episode
 class BotUserSerializer(ModelSerializer):
     class Meta:
         model = BotUserModel
@@ -9,7 +9,13 @@ class TelegramChannelSerializer(ModelSerializer):
         model = TelegramChannelModel
         fields = '__all__'
 
+class EpisodeSerializer(ModelSerializer):
+    class Meta:
+        model = Episode
+        fields = ['id', 'number', 'file_id', 'code', 'created_at']
+
 class MovieSerializer(ModelSerializer):
+    episodes = EpisodeSerializer(many=True, read_only=True)
     class Meta:
         model = Movie
         fields = '__all__'
